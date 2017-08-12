26ae4c44422d6a4a50d1882d507719f23eab8189
cdutten <cdutten@gmail.com>
2017-08-11 22:13:15 -0300

    Seed all the files

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(UsersSeeder::class);
+        $this->call(LaratrustSeeder::class);
+        $this->call(ProductoSeeder::class);
 
     }
 }
